bdd328f86406742ac04693cd00623aefdfbce5c2
mishaShalkow <91632272+mishaShalkow@users.noreply.github.com>
2021-09-30 23:07:09 +0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+let numbers = document.querySelectorAll('.number'),
+    operations = document.querySelectorAll('.operation'),
+    ce = document.getElementById('ce'),
+    c = document.getElementById('c'),
+    decimall = document.getElementById('decimall'),
+    result = document.getElementById('result');
+
+for (let i = 0; i<numbers.length; i++) {
+    let number = numbers[i];
+    number.addEventListener('click', function (e){
+        console.log('klik');
+    });
+}
